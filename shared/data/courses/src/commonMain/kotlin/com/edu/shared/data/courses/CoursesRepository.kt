b90dbe53521d44b8c43cc51ce0ae729374ee376f
Louis Alary <louis.alary@epsi.fr>
2024-11-05 14:23:00 +0100

    [FIX] - Wrong model used to get data

--- shared/data/courses/src/commonMain/kotlin/com/edu/shared/data/courses/CoursesRepository.kt
+++ shared/data/courses/src/commonMain/kotlin/com/edu/shared/data/courses/CoursesRepository.kt
@@ -1,7 +1,7 @@
 package com.edu.shared.data.courses
 
-import com.edu.shared.data.courses.models.CourseModel
+import com.edu.shared.data.courses.models.DayModel
 
 interface CoursesRepository{
-    suspend fun getCourses(): Map<String, CourseModel>
+    suspend fun getCourses(): Map<String, DayModel>
 }
